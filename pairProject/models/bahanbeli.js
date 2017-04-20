752bd193818388d07b2b130910e4a8e0237bed86
diditaditya <di2t.aditya@gmail.com>
2017-04-20 09:03:05 +0700

    Adds association in BahanBeli

--- pairProject/models/bahanbeli.js
+++ pairProject/models/bahanbeli.js
@@ -10,6 +10,8 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+        BahanBeli.belongsTo(models.BahanBaku, {foreignKey: "idBahanBaku"});
+        BahanBeli.belongsTo(models.Pembelian, {foreignKey: "idPembelian"});
       }
     }
   });
