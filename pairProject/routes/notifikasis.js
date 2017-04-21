0814fd46361c6a15a4585f83442246bff2604413
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2017-04-21 09:31:34 +0700

    cek

--- pairProject/routes/notifikasis.js
+++ pairProject/routes/notifikasis.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
+const sisaWaktu = require("../helpers/helpers.js").sisaWaktu;
 
 /* GET users listing. */
 // render halaman daftar notifikasi
 router.get('/', function(req, res, next) {
-    res.render('notifikasis/daftarNotifikasi');
+  models.BahanBeli2.findAll({
+    include: [
+      {
+        model: models.BahanBaku
+      }, {
+        model: models.Pembelian,
+        include: [
+          models.Penyimpanan
+        ]
+      }
+    ]
+  }).then((bahanBelis) => {
+    let array = [];
+    bahanBelis.forEach((bahanBeli) => {
+      let obj = {
+        "id": bahanBeli.id,
+        "jumlah": bahanBeli.jumlah,
+        "tanggalExp": bahanBeli.tanggalExp,
+        "idBahanBaku": bahanBeli.idBahanBaku,
+        "sisaWaktu": sisaWaktu(bahanBeli.tanggalExp, new Date()),
+        "idStatus": bahanBeli.idStatus,
+        "BahanBaku": bahanBeli.BahanBaku,
+        "Pembelian": bahanBeli.Pembelian,
+      };
+      array.push(obj);
+    });
+    // res.json(array);
+    res.render('notifikasis/daftarNotifikasi', {bahanBelis: array});
+  }).catch();
+    // res.render('notifikasis/daftarNotifikasi');
 });
 module.exports = router;
